0843b0158ebdf5377daa899d1174ff3996a29fa5
moonburnt <moonburnt@disroot.org>
2021-03-31 23:00:03 +0300

    style: added license mentions to all modules
    I should've done that long time ago, but always forgot about it

--- Game/spritesheet_cutter.py
+++ Game/spritesheet_cutter.py
@@ -1,3 +1,19 @@
+## a2s3 - action arena game, written in python + panda3d
+## Copyright (c) 2021 moonburnt
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.txt
+
 import logging
 log = logging.getLogger(__name__)
 
